a6ec2b49f7f594e039e1a7e9822b7d7a1d495876
David Gardiner <david@gardiner.net.au>
2017-07-14 18:47:38 +0930

    Copy over

---
+++ PDFXchangeEditor/tools/chocolateyInstall.ps1
@@ -0,0 +1,83 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$packageName = 'PDFXchangeEditor' 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$version    = '6.0.321.0'
+$url        = "http://www.docu-track.co.uk/builds/$version/EditorV6.x86.msi"
+$url64      = "http://www.docu-track.co.uk/builds/$version/EditorV6.x64.msi"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+  silentArgs = '/quiet /norestart'
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'PDF-XChange Editor'
+
+  checksum = '9CF01DFE107BB7DD362078E1F603AE4DD5AE006BB246ABD671AD9DED286B395E'
+  checksumType  = 'sha256' 
+  checksum64 = '3B951FC0E3C20504BA7CB00570D17F3898F20CBFDFB3B1C3E0AAAE18FE821CD7'
+  checksumType64= 'sha256' 
+}
+
+$arguments = @{}
+
+$packageParameters = $env:chocolateyPackageParameters
+
+if ($packageParameters) {
+    $match_pattern = "\/(?<option>([a-zA-Z]+)):(?<value>([`"'])?([a-zA-Z0-9- _\\:\.]+)([`"'])?)|\/(?<option>([a-zA-Z]+))"
+    $option_name = 'option'
+    $value_name = 'value'
+
+    if ($packageParameters -match $match_pattern ){
+        $results = $packageParameters | Select-String $match_pattern -AllMatches
+        $results.matches | % {
+        $arguments.Add(
+            $_.Groups[$option_name].Value.Trim(),
+            $_.Groups[$value_name].Value.Trim())
+    }
+    }
+    else
+    {
+        Throw "Package Parameters were found but were invalid (REGEX Failure)"
+    }
+
+    # http://help.tracker-software.com/EUM/default.aspx?pageid=PDFXEdit3:switches_for_msi_installers
+    $customArguments = @{}
+
+    if ($arguments.ContainsKey("NoDesktopShortcuts")) {
+        Write-Host "You want NoDesktopShortcuts"
+        $customArguments.Add("DESKTOP_SHORTCUTS", "0")
+    }
+
+    if ($arguments.ContainsKey("NoUpdater")) {
+        Write-Host "You want NoUpdater"
+        $customArguments.Add("NOUPDATER", "1")
+    }
+
+    if ($arguments.ContainsKey("NoViewInBrowsers")) {
+        Write-Host "You want NoViewInBrowsers"
+        $customArguments.Add("VIEW_IN_BROWSERS", "0")
+    }
+
+    if ($arguments.ContainsKey("NoSetAsDefault")) {
+        Write-Host "You want NoSetAsDefault"
+        $customArguments.Add("SET_AS_DEFAULT", "0")
+    }
+
+    if ($arguments.ContainsKey("NoProgramsMenuShortcuts")) {
+        Write-Host "You want NoProgramsMenuShortcuts"
+        $customArguments.Add("PROGRAMSMENU_SHORTCUTS", "0")
+    }
+
+} else {
+    Write-Debug "No Package Parameters Passed in"
+}
+
+if ($customArguments.Count) { 
+    $packageArgs.silentArgs += " " + (($customArguments.GetEnumerator() | % { "$($_.Name)=$($_.Value)" } ) -join " ")
+}
+
+Install-ChocolateyPackage @packageArgs
