307fc59e38bd4df0213f0e8f05fce15c06de2a65
David Gardiner <david@gardiner.net.au>
2017-09-06 22:06:10 +0930

    Add uninstall script

--- keepass-rpc/tools/chocolateyinstall.ps1
+++ keepass-rpc/tools/chocolateyinstall.ps1
@@ -1,5 +1,4 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 if (Test-Path env:"ProgramFiles(x86)")
 {
@@ -8,14 +7,14 @@ if (Test-Path env:"ProgramFiles(x86)")
   $fileFullPath = $env:ProgramFiles
 }
 
-$fileFullPath = [IO.Path]::Combine($fileFullPath, "KeePassRPC.plgx")
+$fileFullPath = [IO.Path]::Combine($fileFullPath, "KeePass Password Safe 2\Plugins\KeePassRPC.plgx")
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   url           = "https://github.com/kee-org/keepassrpc/releases/download/v1.7.2/KeePassRPC.plgx"
   checksum      = '5196D04D1E395FA51EC535EF213F22128156811F8FCE079C89974D0845FF60A8'
   checksumType  = 'sha256'
-  FileFullPath   = fileFullPath
+  FileFullPath   = $fileFullPath
 }
 
 Get-ChocolateyWebFile @packageArgs
