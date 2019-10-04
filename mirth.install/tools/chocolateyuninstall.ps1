542b2f56a89d9460d155fe8414795034f223c657
David Gardiner <david@gardiner.net.au>
2019-10-04 16:06:40 +0930

    Uninstall with console
    - use ProgramData

--- mirth.install/tools/chocolateyuninstall.ps1
+++ mirth.install/tools/chocolateyuninstall.ps1
@@ -3,14 +3,14 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'Mirth Connect*'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
   fileType      = 'EXE' #only one of these: MSI or EXE (ignore MSU for now)
-  silentArgs   = '-q'           # Install4j
+  silentArgs   = '-q -console'
   validExitCodes= @(0) #please insert other valid exit codes here
 }
 
 [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
 if ($key.Count -eq 1) {
-  $key | % { 
+  $key | ForEach-Object { 
     $packageArgs['file'] = "$($_.UninstallString)" #NOTE: You may need to split this if it contains spaces, see below
     
     if ($packageArgs['fileType'] -eq 'MSI') {
@@ -39,5 +39,5 @@ if ($key.Count -eq 1) {
   Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $($_.DisplayName)"}
+  $key | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
 }
