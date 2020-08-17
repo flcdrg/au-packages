c06945b478e27b5d53d34020f7a212f0cf8d95bc
flcdrg <david@gardiner.net.au>
2020-08-17 00:06:51 +0000

    AU: 1 updated - lenovo-thinkvantage-system-update [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/eee5d70fedcba536fe48698c2fcfecae430333dd

--- lenovo-thinkvantage-system-update/tools/chocolateyinstall.ps1
+++ lenovo-thinkvantage-system-update/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'lenovo-thinkvantage-system-update'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.lenovo.com/pccbbs/thinkvantage_en/system_update_5.07.0097.exe'
-$checksum   = 'b196c42c8d314a9e593f4cb5bced3e813b7742852e31e8da782c624b76bc61fa'
+$url        = 'https://download.lenovo.com/pccbbs/thinkvantage_en/system_update_5.07.0106.exe'
+$checksum   = 'cd10a6a737575b2c95baf26b07f546dc19621f7f60b569130e9b3986bcc31660'
 
 $packageArgs = @{
   packageName   = $packageName
