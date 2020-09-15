9bc9db91ede27fdd8745f870425c349f3b62d332
David Gardiner <david@gardiner.net.au>
2020-09-15 13:36:22 +0930

    Mirth 3.9.1

--- mirth.install/tools/chocolateyinstall.ps1
+++ mirth.install/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://s3.amazonaws.com/downloads.mirthcorp.com/connect/3.9.0.b2526/mirthconnect-3.9.0.b2526-windows.exe'
-$url64      = 'https://s3.amazonaws.com/downloads.mirthcorp.com/connect/3.9.0.b2526/mirthconnect-3.9.0.b2526-windows-x64.exe'
+$url        = 'https://s3.amazonaws.com/downloads.mirthcorp.com/connect/3.9.1.b263/mirthconnect-3.9.1.b263-windows.exe'
+$url64      = 'https://s3.amazonaws.com/downloads.mirthcorp.com/connect/3.9.1.b263/mirthconnect-3.9.1.b263-windows-x64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -13,9 +13,9 @@ $packageArgs = @{
 
   softwareName  = 'Mirth Connect *'
 
-  checksum      = '6e62ba2df73eeb7416fc8d18f9945697b8064661d3506e41c83d5e1ed79f5fe0'
+  checksum      = '632e5ecab89dcacebf777b38ec2fd63f280550539082572b6de4ce30f304e360'
   checksumType  = 'sha256'
-  checksum64    = '9845791f4f4437c2f0e77486716f920fb81ec9f9f6d893d88007437e578fb017'
+  checksum64    = 'a90fd803a88c4dae266aac0ddfa48d1da538cf7c622a5fd459ba217549a5a641'
   checksumType64= 'sha256'
 
   # OTHERS
