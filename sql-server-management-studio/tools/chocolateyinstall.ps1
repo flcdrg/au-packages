0844abcbd383fdcc2829e3efc0762d5c1604d05e
flcdrg <david@gardiner.net.au>
2020-10-30 00:07:27 +0000

    AU: 11 updated - dotCover dotMemory dotPeek dotTrace dotUltimate ReSharper ReSharperCpp resharper-ultimate-all sql-server-management-studio ssms teamCityAddin [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6

--- sql-server-management-studio/tools/chocolateyinstall.ps1
+++ sql-server-management-studio/tools/chocolateyinstall.ps1
@@ -3,15 +3,15 @@
 $packageName= 'SQL Server Management Studio'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$fullUrl = 'https://download.microsoft.com/download/d/9/7/d9789173-aaa7-4f5b-91b0-a2a01f4ba3a6/SSMS-Setup-ENU.exe'
-$fullChecksum = '4AFF3195BA739BBA31B77FE8B3C58DBDD49AF25C87704D9C77B26017AEE1CE33'
+$fullUrl = 'https://download.microsoft.com/download/2/d/1/2d12f6a1-e28f-42d1-9617-ac036857c5be/SSMS-Setup-ENU.exe'
+$fullChecksum = '27FBEAD01257513F87C619765C06611B2F4F53C7C6C2B9F1DC3EDF3ACE14295D'
 
 # Upgrading is commented out for 18.x until the first upgrade package is made available by Microsoft
 
 # $upgradeUrl = 'https://download.microsoft.com/download/D/D/4/DD495084-ADA7-4827-ADD3-FC566EC05B90/SSMS-Setup-ENU-Upgrade.exe'
 # $upgradeChecksum = 'A092B5F4270F19B83874E8ECDEC8EF309B8DBC55462E99EAFA309A75A1D04E09'
 
-$release = '18.6'
+$release = '18.7.1'
 
 # Check if 17.0 is installed so we can get upgrade package instead of full package
 # $version17 = [version]"14.0.17099.0"
