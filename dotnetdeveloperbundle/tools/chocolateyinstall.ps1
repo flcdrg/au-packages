c8643f81cb2faff9944d4567718e25dee6b39d29
flcdrg <david@gardiner.net.au>
2020-11-25 00:07:17 +0000

    AU: 1 updated - dotnetdeveloperbundle [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/9978df7553161e55bec6f7f79539c96d43e13e1c

--- dotnetdeveloperbundle/tools/chocolateyinstall.ps1
+++ dotnetdeveloperbundle/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$checksum = '63160B3C2FDBF259F0E19F38C384D14BBC0CA89C44186799A9430D7ADE372AF9'
-$url = 'https://download.red-gate.com/installers/DotNETDeveloperBundle/2020-11-18/DotNETDeveloperBundle.exe'
+$checksum = '4F10863E271E8EB593A2A103046957969F1C52CFD577CB2017D4694811172048'
+$url = 'https://download.red-gate.com/installers/DotNETDeveloperBundle/2020-11-23/DotNETDeveloperBundle.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
