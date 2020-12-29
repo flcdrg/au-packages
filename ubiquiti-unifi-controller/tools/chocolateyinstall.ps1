8578b56caef696d14f86ae1ed31a78176259da07
flcdrg <david@gardiner.net.au>
2020-12-29 00:06:59 +0000

    AU: 1 updated - ubiquiti-unifi-controller [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/a502c17ff1ee64339ee4fbd42d0209990f48423a

--- ubiquiti-unifi-controller/tools/chocolateyinstall.ps1
+++ ubiquiti-unifi-controller/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'unifi-controller'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.ubnt.com/downloads/unifi/6.0.41/UniFi-installer.exe'
-$checksum   = '71edad4a515d553f237eacb0e7ab4a85254ba31aa972f1f1bce57c90affd52dc'
+$url        = 'https://www.ubnt.com/downloads/unifi/6.0.43/UniFi-installer.exe'
+$checksum   = '5910c36e0e5ccb969e61ff6eacdbc184376358f63b6a46c0e6715588f5d0ef44'
 
 $packageArgs = @{
   packageName   = $packageName
