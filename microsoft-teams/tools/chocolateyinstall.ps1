fb7e04e1971261d28f41fb7cdd63b78fc9f382de
flcdrg <david@gardiner.net.au>
2021-01-30 00:07:49 +0000

    AU: 3 updated - microsoft-teams TeamCity-OpenJDK8 TeamCity-PreinstalledJRE [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/442998027ea7c9389ef2841499500a2c1c94eaba

--- microsoft-teams/tools/chocolateyinstall.ps1
+++ microsoft-teams/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 
 $packageName= 'microsoft-teams'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://statics.teams.cdn.office.net/production-windows/1.3.00.34662/Teams_windows.exe'
-$url64      = 'https://statics.teams.cdn.office.net/production-windows-x64/1.3.00.34662/Teams_windows_x64.exe'
-$checksum32 = '403733228fb77e74e7e7350cc1da7834862f3c337893d18ad378cf9cb48bb563'
-$checksum64 = 'cdb41bfbb8107048f4fcdfe230ce9faf138635d87284206ec6c10624533bf1e9'
+$url32      = 'https://statics.teams.cdn.office.net/production-windows/1.4.00.2781/Teams_windows.exe'
+$url64      = 'https://statics.teams.cdn.office.net/production-windows-x64/1.4.00.2781/Teams_windows_x64.exe'
+$checksum32 = '26f23e3a03098ad67b03e1f3322ab1178bc6853c9e135a238ef0853fcf40f8e2'
+$checksum64 = '695344aad632dde7ff022b679b291b00370fe7adca2b17665a6b63e3ca5fd637'
 
 $packageArgs = @{
   packageName   = $packageName
