98ca3bc48d8c047d5488753ea853e2160ffa19e1
David Gardiner <david@gardiner.net.au>
2021-02-03 11:06:41 +1030

    Working package
    - disable update.ps1 for now until first version approved

--- stripe-cli/_update.ps1
+++ stripe-cli/_update.ps1
@@ -1,21 +1,22 @@
 import-module au
 
-# function global:au_SearchReplace {
-#     @{
-#         'tools\chocolateyInstall.ps1' = @{
-#             "(^\`$url\s*=\s*)('.*')"    = "`$1'$($Latest.Url32)'"
-#             "(^\`$url64\s*=\s*)('.*')"   = "`$1'$($Latest.Url64)'"
-#             "(^\`$checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-#             "(^\`$checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
-#         }
-#     }
-# }
+function global:au_SearchReplace {
+    @{
+    }
+}
 
 . ../_scripts/GitHub.ps1
 
 function global:au_BeforeUpdate() {
-    #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
+
     Get-RemoteFiles -Purge
+
+    # Just include .exe in package rather than zip which we would then need to unzip
+    Push-Location tools
+    Get-ChildItem *.exe | Remove-Item -Force
+    Expand-Archive $global:Latest.FileName32 -DestinationPath .
+    Remove-Item $global:Latest.FileName32 -Force
+    Pop-Location
 }
 
 function global:au_GetLatest {
@@ -32,11 +33,6 @@ function global:au_GetLatest {
     $asset = $assets | Where-Object { $_.name.EndsWith("windows_x86_64.zip") } | Select-Object -First 1
     # stripe_1.5.8_windows_x86_64.zip
     # https://github.com/stripe/stripe-cli/releases/download/v1.5.8/stripe_1.5.8_windows_x86_64.zip
-
-    
-
-    # Note at some point we may want to offer the non-node installers as well, which use 'pipeline-agent' as a prefix
-    # See https://github.com/microsoft/azure-pipelines-agent/pull/3170 for more context
 
     $Latest = @{
         URL32 = $asset.browser_download_url
