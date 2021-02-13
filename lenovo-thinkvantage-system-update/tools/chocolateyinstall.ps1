5364fc0d5d8b101daca0e7903fe6e2b21847224b
David Gardiner <david@gardiner.net.au>
2021-02-13 20:00:32 +1030

    Manually bump Lenovo package

--- lenovo-thinkvantage-system-update/tools/chocolateyinstall.ps1
+++ lenovo-thinkvantage-system-update/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'lenovo-thinkvantage-system-update'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.lenovo.com/pccbbs/thinkvantage_en/system_update_5.07.0113.exe'
-$checksum   = 'f7ffd0edab62684de211388d65b598cd7859cba72ae051743a38d3afe8d922f4'
+$url        = 'https://download.lenovo.com/pccbbs/thinkvantage_en/system_update_5.07.0117.exe'
+$checksum   = '69e5a97904df3fc7fb897c73a08bcc81cd7a5778f15ce22a4c8010d2995d3d96'
 
 $packageArgs = @{
   packageName   = $packageName
