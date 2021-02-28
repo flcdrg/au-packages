afb452763d36fef8a6c2828880bb2501539d48bf
flcdrg <david@gardiner.net.au>
2021-02-28 00:06:59 +0000

    AU: 1 updated - dobi [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/d991d7f3a1e6782485e671e9fac8867e37c60b7a

--- dobi/tools/chocolateyinstall.ps1
+++ dobi/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url = 'https://github.com/dnephin/dobi/releases/download/v0.14.0/dobi-windows.exe'
-$checksum = '90b02de565936155bb03affa29a4e2fb9470f86adb5641f4805411db751a05d8'
+$url = 'https://github.com/dnephin/dobi/releases/download/v0.15.0/dobi-windows.exe'
+$checksum = 'b2c1d1e580cca5d4bd5e9209a9ed6ec2c9fb079fb03835d515e226b5d76915a3'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
