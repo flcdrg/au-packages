b46771a9805c03ab1ff4e0b1f2c32c7155ef60cc
David Gardiner <david@gardiner.net.au>
2021-05-27 08:52:43 +0930

    Add VirusTotal for resharper-platform

--- resharper-platform/update.ps1
+++ resharper-platform/update.ps1
@@ -19,4 +19,23 @@ function global:au_GetLatest {
     return GetJetbrainsReSharperPlatformLatest
 }
 
+function global:au_AfterUpdate ($Package)  {
+
+    if ($Package.RemoteVersion -ne $Package.NuspecVersion) {
+
+        Get-RemoteFiles -NoSuffix
+
+        $file = [IO.Path]::Combine("tools", $Latest.FileName32)
+
+        Write-Output "Submitting file $file to VirusTotal"
+
+        # Assumes vt-cli Chocolatey package is installed!
+        vt.exe scan file $file --apikey $env:VT_APIKEY
+
+        Remove-Item $file -ErrorAction Ignore
+
+        $Latest.Remove("FileName32")
+    }
+}
+
 update -ChecksumFor none
