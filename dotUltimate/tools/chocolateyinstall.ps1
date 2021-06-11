c6936689caf9b4548dd8e4ef5cb74592ccc6d60a
David Gardiner <david@gardiner.net.au>
2021-06-11 10:03:48 +0930

    Add more parameters to dotUltimate package

--- dotUltimate/tools/chocolateyinstall.ps1
+++ dotUltimate/tools/chocolateyinstall.ps1
@@ -8,17 +8,41 @@ $filename = 'JetBrains.dotUltimate.2021.2.EAP2.Checked.exe'
 $installPath = Join-Path  (Join-Path $commonPath $platformPackageName) $filename
 $packageParameters = Get-PackageParameters
 
-$products = "ReSharper;dotTrace;dotMemory;dotCover;dotPeek;Rider"
+$products = @()
+
+if ($null -eq $packageParameters["NoReSharper"]) {
+  $products += "ReSharper"
+}
+
+if ($null -eq $packageParameters["NoDotTrace"]) {
+  $products += "dotTrace"
+}
+
+if ($null -eq $packageParameters["NoDotMemory"]) {
+  $products += "dotMemory"
+}
+
+if ($null -eq $packageParameters["NoDotCover"]) {
+  $products += "dotCover"
+}
+
+if ($null -eq $packageParameters["NoDotPeek"]) {
+  $products += "dotPeek"
+}
 
 if ($null -eq $packageParameters["NoCpp"]) {
-  $products += ";ReSharperCpp"
+  $products += "ReSharperCpp"
 }
 
 if ($null -eq $packageParameters["NoTeamCityAddin"]) {
-  $products += ";teamCityAddin"
+  $products += "teamCityAddin"
 }
 
-$silentArgs = "/Silent=True /SpecificProductNames=$products /VsVersion=*"
+if ($null -eq $packageParameters["NoRider"]) {
+  $products += "Rider"
+}
+
+$silentArgs = "/Silent=True /SpecificProductNames=$($products -join ';') /VsVersion=*"
 
 Write-Verbose $silentArgs
 
