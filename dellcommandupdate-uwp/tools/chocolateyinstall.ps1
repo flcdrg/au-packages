192f939e9525c2ed91ae68c95eb6b2698bb5a555
David Gardiner <david@gardiner.net.au>
2021-06-17 15:06:29 +0930

    Dell Command Update UWP 4.2.1

--- dellcommandupdate-uwp/tools/chocolateyinstall.ps1
+++ dellcommandupdate-uwp/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://dl.dell.com/FOLDER07337375M/1/Dell-Command-Update-Application-for-Windows10_JDXHH_WIN_4.2.0_A00.EXE'
-$checksum = '38bc35d6eb70f020c567588b454a92d10f2041d2b15c65cd28328fa0b5c0b228'
+$url = 'https://dl.dell.com/FOLDER07414802M/1/Dell-Command-Update-Application-for-Windows-10_W1RMW_WIN_4.2.1_A00.EXE'
+$checksum = '552d37a1dd5ef06c7af82f8f81f003e9f4b35d6d95ecaf2361c84f43459630f3'
 
  # the /factoryinstall parameter makes installer return success instead of '2' (which Chocolatey doesn't like)
 $packageArgs = @{
