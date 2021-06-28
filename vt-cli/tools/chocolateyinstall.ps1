09c5ae984288ef9ab65d94f8d06cdbb91e0e1e8f
David Gardiner <david@gardiner.net.au>
2021-06-28 21:11:47 +0930

    Add vt-cli

---
+++ vt-cli/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/VirusTotal/vt-cli/releases/download/0.9.6/Windows32.zip'
+$url64      = 'https://github.com/VirusTotal/vt-cli/releases/download/0.9.6/Windows64.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'vt-cli*'
+  checksum      = '1B858DDE065C95C94AF5735717B19FEA0A2901827B9239E2248034830CCD0EE9'
+  checksumType  = 'sha256'
+  checksum64    = 'D07AE25993FB21E439A05BAB33899228CDDF731147D3A3C0865E4643751C8685'
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
