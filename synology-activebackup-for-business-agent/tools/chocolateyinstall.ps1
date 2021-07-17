8a748a8ed9b9b08436475f934e371b1568cd4d2d
David Gardiner <david@gardiner.net.au>
2021-07-17 10:03:36 +0930

    Synology ActiveBackup for Business Agent 2.2.0.2070

--- synology-activebackup-for-business-agent/tools/chocolateyinstall.ps1
+++ synology-activebackup-for-business-agent/tools/chocolateyinstall.ps1
@@ -42,11 +42,11 @@ Write-Output $silentArgs
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'msi'
-    url            = "https://global.download.synology.com/download/Tools/ActiveBackupBusinessAgent/2.1.1-1125/Windows/i686/Synology%20Active%20Backup%20for%20Business%20Agent-2.1.1-1125-x86.msi"
-    url64          = "https://global.download.synology.com/download/Tools/ActiveBackupBusinessAgent/2.1.1-1125/Windows/x86_64/Synology%20Active%20Backup%20for%20Business%20Agent-2.1.1-1125-x64.msi"
-    checksum       = "DAC9DBA09FA2D06A0D9B9DF15B5AA58D8CB112041148677EC7069B16D4423B30031A77C984B81551FEB71F362DFDC34653EA9B9673678350E161830564C8FCA9"
+    url            = "https://global.download.synology.com/download/Utility/ActiveBackupBusinessAgent/2.2.0-2070/Windows/i686/Synology%20Active%20Backup%20for%20Business%20Agent-2.2.0-2070-x86.msi"
+    url64          = "https://global.download.synology.com/download/Utility/ActiveBackupBusinessAgent/2.2.0-2070/Windows/x86_64/Synology%20Active%20Backup%20for%20Business%20Agent-2.2.0-2070-x64.msi"
+    checksum       = "33C52D4C5C8F55099CA56395F73C3050B3571ED0F5F390E4AE49687DBDB1CF298F7F28FE5FE77D99FF3AC30045F6DCFC4678BDB97F288508D8D6576E495C16A5"
     checksumType   = 'SHA512'
-    checksum64     = "1459A3458BB71262765446040A1DC228808FCF176C1794ED33D6681114454ED7C3CC8687B09AA9810F053F59FF3151B88EA98BA87F0FD24D62D1BAE3484499DA"
+    checksum64     = "AA95FBBE1CDFB70424109115F23E2A73485F28A481543ABB3C0251B49848A0D6A8540B3F604ED358C59C93DE8C7E4520AD9630E000EFAC8A2489BB3EC5D3F6C6"
     checksumType64 = 'SHA512'
     silentArgs     = "$($silentArgs) /qn"
     ValidExitCodes = @(0, 1000, 1101)
