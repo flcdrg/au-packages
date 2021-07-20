88b0181fe1cd024f83a50b9a8d0cdfa752cca28f
flcdrg <david@gardiner.net.au>
2021-07-20 00:07:57 +0000

    AU: 10 updated - dotCover dotMemory dotPeek dotTrace dotUltimate ReSharper resharper-clt ReSharperCpp resharper-ultimate-all teamCityAddin [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/808ce81c47c237cad5238bc233b9097f4d47dc95

--- dotUltimate/tools/chocolateyuninstall.ps1
+++ dotUltimate/tools/chocolateyuninstall.ps1
@@ -4,7 +4,7 @@ $platformPackageName = 'resharper-platform'
 $packageName = 'dotUltimate'
 $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
-$filename = 'JetBrains.dotUltimate.2021.1.4.exe'
+$filename = 'JetBrains.dotUltimate.2021.2.EAP7.Checked.exe'
 $installPath = Join-Path  (Join-Path $commonPath $platformPackageName) $filename
 
 $silentArgs = '/Silent=True /SpecificProductNamesToRemove=* /VsVersion=* /ReSharper9PlusMsi=True'
