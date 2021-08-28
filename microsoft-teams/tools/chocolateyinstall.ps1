666c8007ce051efc2fbeb580c98d4e174c16601a
flcdrg <david@gardiner.net.au>
2021-08-28 00:09:58 +0000

    AU: 11 updated - dotCover dotMemory dotPeek dotTrace dotUltimate microsoft-teams ReSharper resharper-clt ReSharperCpp resharper-ultimate-all teamCityAddin [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/c269d3755d356a9eda78342387ea3db24391cef9

--- microsoft-teams/tools/chocolateyinstall.ps1
+++ microsoft-teams/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 
 $packageName= 'microsoft-teams'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://statics.teams.cdn.office.net/production-windows/1.4.00.19572/TeamsSetup.exe'
-$url64      = 'https://statics.teams.cdn.office.net/production-windows-x64/1.4.00.19572/Teams_windows_x64.exe'
-$checksum32 = 'b4f79a1e48aaab0206ab341874f965a088138905c7990a7944c469eb73975c19'
-$checksum64 = '1d96b9f3ef5b99d9418d1b6e690db38d5493fcfda636f41f271bc91eedcc9c7c'
+$url32      = 'https://statics.teams.cdn.office.net/production-windows/1.4.00.22472/TeamsSetup.exe'
+$url64      = 'https://statics.teams.cdn.office.net/production-windows-x64/1.4.00.22472/Teams_windows_x64.exe'
+$checksum32 = '7cd5eeb1ce0e10da769f85bb8462b50715ea0f34c4bc2240600baeb50ed5b3cf'
+$checksum64 = 'd75ef07ec26a980a30a3e44fc1a3539916e6198d82b0cf31210e6a856472b13b'
 
 $packageArgs = @{
   packageName   = $packageName
