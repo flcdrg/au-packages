015275a4628609b3188dd207449e48105e0672d0
flcdrg <david@gardiner.net.au>
2021-10-28 00:08:12 +0000

    AU: 1 updated - sql-server-2017-cumulative-update [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/0bd67bb38d650221b5000437dc7a9b3c41969d02

--- sql-server-2017-cumulative-update/tools/chocolateyinstall.ps1
+++ sql-server-2017-cumulative-update/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://download.microsoft.com/download/C/4/F/C4F908C9-98ED-4E5F-88D5-7D6A5004AEBD/SQLServer2017-KB5005226-x64.exe'
-$checksum   = '7b48b534bc874acd7d6b6e58646487ebfb6b354bccbff758bafba8807c477e8b'
-$softwareName = 'Hotfix 3411 for SQL Server 2017*(KB5005226)*'
+$url        = 'https://download.microsoft.com/download/C/4/F/C4F908C9-98ED-4E5F-88D5-7D6A5004AEBD/SQLServer2017-KB5006944-x64.exe'
+$checksum   = 'ccfa4dc8c7d39b2c3bc5f97067c8c57e72d7f9180ad5aabcc0b5d86e41513e66'
+$softwareName = 'Hotfix 3421 for SQL Server 2017*(KB5006944)*'
 
 [bool] $runningAU = (Test-Path Function:\au_GetLatest)
 $filename = [IO.Path]::GetFileName($url)
