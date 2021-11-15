8af1371aa5eca78ed9d71f53202f3e8c52ea1135
David Gardiner <david@gardiner.net.au>
2021-11-15 16:55:29 +1030

    Update how we look for plugin folder
    Fixes #106

--- keepass-yet-another-favicon-downloader/tools/chocolateyinstall.ps1
+++ keepass-yet-another-favicon-downloader/tools/chocolateyinstall.ps1
@@ -3,13 +3,12 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'YetAnotherFaviconDownloader.plgx'
 
-if (Test-Path env:"ProgramFiles(x86)")
-{
-  $fileFullPath = ${env:ProgramFiles(x86)}
-} else {
-  $fileFullPath = $env:ProgramFiles
+. "${toolsDir}\common.ps1"
+
+# Now copy the plugin into the KeePass plugins directory
+$fileFullPath = Get-KeePassPluginsPath
+
+if (-not (Test-Path $fileFullPath)) {
+  New-Item -ItemType Directory $fileFullPath | Out-Null
 }
-
-$fileFullPath = [IO.Path]::Combine($fileFullPath, "KeePass Password Safe 2\Plugins\YetAnotherFaviconDownloader.plgx")
-
 Copy-Item -Path $fileLocation -Destination $fileFullPath
