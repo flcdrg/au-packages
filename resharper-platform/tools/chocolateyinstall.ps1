9010a22107fce41f50b6af28d0e9ba1cfa617bae
flcdrg <david@gardiner.net.au>
2021-12-02 00:07:06 +0000

    AU: 1 updated - resharper-platform [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/1a7f6b3aec721079e0fd44dc4c7f04a1e57091c3

--- resharper-platform/tools/chocolateyinstall.ps1
+++ resharper-platform/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$filename = 'JetBrains.dotUltimate.2021.3.EAP9.Checked.exe'
-$checksum = '3e6ac08537e71f2ca138d94a9853faf21fe5509cb7f16857822ad4aa5cb27327'
+$filename = 'JetBrains.dotUltimate.2021.3.EAP10.Checked.exe'
+$checksum = '440f959fd43f2b245182321cd4559b489c0196222dbbe6eb1ff73e0a56aeef1b'
 
-$url = 'https://download.jetbrains.com/resharper/dotUltimate.2021.3.EAP9/JetBrains.dotUltimate.2021.3.EAP9.Checked.exe'
+$url = 'https://download.jetbrains.com/resharper/dotUltimate.2021.3.EAP10/JetBrains.dotUltimate.2021.3.EAP10.Checked.exe'
 $packagePath = $(Split-Path -parent $toolsDir)
 $installPath = Join-Path $packagePath $filename
 
