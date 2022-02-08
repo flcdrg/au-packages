riezebosch/BoxstarterPackages

The script tries to grab the SQL Server 2016 uninstall string, but that won't exist for 2017.

sql-server-express/tools/chocolateyuninstall.ps1
@@ -3,5 +3,5 @@
 $packageName= 'sql-server-express'
 $silentArgs = ""
 
-$setupPath = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft SQL Server SQLServer2016').UninstallString
+$setupPath = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft SQL Server SQL').UninstallString
 Uninstall-ChocolateyPackage "$packageName" "EXE" "$silentArgs" "$setupPath" -validExitCodes @(0, 3010)

flcdrg/au-packages

sql-server-express/tools/chocolateyuninstall.ps1
@@ -3,5 +3,5 @@
 $packageName= 'sql-server-express'
 $silentArgs = ""
 
-$setupPath = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft SQL Server SQLServer2016').UninstallString
+$setupPath = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft SQL Server SQL2017').UninstallString
 Uninstall-ChocolateyPackage "$packageName" "EXE" "$silentArgs" "$setupPath" -validExitCodes @(0, 3010)