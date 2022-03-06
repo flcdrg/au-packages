f75e3576843aca976ca7c09bed0b7d04a1b4cf38
David Gardiner <david@gardiner.net.au>
2022-03-06 21:18:15 +1030

    Oscar 1.3.1

--- oscar-cpap-analysis/tools/chocolateyinstall.ps1
+++ oscar-cpap-analysis/tools/chocolateyinstall.ps1
@@ -9,11 +9,11 @@ $packageArgs = @{
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   
   validExitCodes= @(0) #please insert other valid exit codes here
-  url           = "https://www.apneaboard.com/OSCAR/OSCAR-1.2.0-Win32.exe"
-  checksum      = 'D85758031D51DAB7FD72B46D7C76FB574C357DDD00AAE7FC51F828725B841FC1'
+  url           = "https://www.apneaboard.com/OSCAR/OSCAR-1.3.1-Win32.exe"
+  checksum      = 'E6CCD57729521B3452CAC6F10584DADEB29B9AF2F9D2A178897530F2A8DAC370'
   checksumType  = 'sha256'
-  url64bit      = "https://www.apneaboard.com/OSCAR/OSCAR-1.2.0-Win64.exe"
-  checksum64    = 'F6968F75FC8C398A6A26BAED84AA5F14358F284ED8FECC00797906FFABFF1EC2'
+  url64bit      = "https://www.apneaboard.com/OSCAR/OSCAR-1.3.1-Win64.exe"
+  checksum64    = '8F393F30C051B320E725F25EFEBB31D59F0ED3B0F35EE02BB480658CF3DF9A75'
   checksumType64= 'sha256'
   destination   = $toolsDir
 }
