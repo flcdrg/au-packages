9383b69de031589c02512488d43dba1d989c3e32
flcdrg <david@gardiner.net.au>
2022-04-29 00:07:50 +0000

    AU: 1 updated - teamcity [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/c17b6acdd4947a135b79179ae9c32d44b6c414fc

--- teamcity/tools/chocolateyInstall.ps1
+++ teamcity/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $filename = "TeamCity-$($env:ChocolateyPackageVersion).tar.gz"
-$checksum = '2b0c5650cb5b007a5a195fe9d393485e9ae7ba0e9850245602a31bc24522bc72'
+$checksum = 'f137496cd8b38208971149e97f6a5a845a08ee73b8ebc1b03fa64d41c38fdee5'
 
 $url = "https://download.jetbrains.com/teamcity/TeamCity-$($env:ChocolateyPackageVersion).tar.gz"
 $packagePath = $(Split-Path -parent $toolsDir)
