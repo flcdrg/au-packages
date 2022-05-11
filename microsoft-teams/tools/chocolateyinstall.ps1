905d7d5965e60097465c304fd4cc1d35b3b75267
flcdrg <david@gardiner.net.au>
2022-05-11 00:09:01 +0000

    AU: 2 updated - iguana microsoft-teams [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/857337a376b2160fcb7346e759d15c77d6412bf5

--- microsoft-teams/tools/chocolateyinstall.ps1
+++ microsoft-teams/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 
 $packageName= 'microsoft-teams'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://statics.teams.cdn.office.net/production-windows/1.5.00.9163/TeamsSetup.exe'
-$url64      = 'https://statics.teams.cdn.office.net/production-windows-x64/1.5.00.9163/Teams_windows_x64.exe'
-$checksum32 = 'ab24a87774a92c8da866c11f69f94e5a262eb177e708a0f09a0a19e98f80949f'
-$checksum64 = '357474891f58c2148d5fe2000bd061d1c10e2ba326efc5ef116f1a121eb314e0'
+$url32      = 'https://statics.teams.cdn.office.net/production-windows/1.5.00.11163/TeamsSetup.exe'
+$url64      = 'https://statics.teams.cdn.office.net/production-windows-x64/1.5.00.11163/Teams_windows_x64.exe'
+$checksum32 = 'f30ffbdca926b7b6cfeae39204100873485aa2852f5886d6cfaeeb133649f636'
+$checksum64 = '9ccad9e1d6a24821aa01a976fb956fdd09b800baeb79c27ca86ba587e06c5922'
 
 $packageArgs = @{
   packageName   = $packageName
