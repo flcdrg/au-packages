5b26f9eae185709eb1ba0b41607f33af50fe67b1
David Gardiner <david@gardiner.net.au>
2022-05-24 11:14:34 +0930

    ignore version errors

--- iguana.install/update.ps1
+++ iguana.install/update.ps1
@@ -14,14 +14,21 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases #1 
-    $url64 = $download_page.links | Where-Object href -match 'iguana_\d.*x64.zip$' | Select-Object -First 1 -expand href
+    try {
+        $download_page = Invoke-WebRequest -Uri $releases #1 
+        $url64 = $download_page.links | Where-Object href -match 'iguana_\d.*x64.zip$' | Select-Object -First 1 -expand href
 
-    # http://dl.interfaceware.com/iguana/windows/6_0_6/iguana_6_0_6_windows_x64.exe
-    $version = ($url64 -split '/' | Select-Object -Skip 5 -First 1) -replace '_', '.'
+        # http://dl.interfaceware.com/iguana/windows/6_0_6/iguana_6_0_6_windows_x64.exe
+        $version = ($url64 -split '/' | Select-Object -Skip 5 -First 1) -replace '_', '.'
 
-    $Latest = @{ URL64 = $url64; Version = $version }
-    return $Latest
+        $Latest = @{ URL64 = $url64; Version = $version }
+        return $Latest
+    }
+    catch {
+        Write-Warning $_
+        return 'ignore'
+    }
+
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
