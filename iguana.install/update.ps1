16bc1c244be8fdbe81987c4d23d0d04d440ee875
David Gardiner <david@gardiner.net.au>
2022-05-24 11:45:57 +0930

    ignore if no download url found

--- iguana.install/update.ps1
+++ iguana.install/update.ps1
@@ -14,21 +14,20 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    try {
-        $download_page = Invoke-WebRequest -Uri $releases #1 
-        $url64 = $download_page.links | Where-Object href -match 'iguana_\d.*x64.zip$' | Select-Object -First 1 -expand href
 
-        # http://dl.interfaceware.com/iguana/windows/6_0_6/iguana_6_0_6_windows_x64.exe
-        $version = ($url64 -split '/' | Select-Object -Skip 5 -First 1) -replace '_', '.'
+    $download_page = Invoke-WebRequest -Uri $releases #1 
+    $url64 = $download_page.links | Where-Object href -match 'iguana_\d.*x64.zip$' | Select-Object -First 1 -expand href
 
-        $Latest = @{ URL64 = $url64; Version = $version }
-        return $Latest
-    }
-    catch {
-        Write-Warning $_
+    if (-not $url64) {
+        Write-Warning "Download URL not found"
         return 'ignore'
     }
 
+    # http://dl.interfaceware.com/iguana/windows/6_0_6/iguana_6_0_6_windows_x64.exe
+    $version = ($url64 -split '/' | Select-Object -Skip 5 -First 1) -replace '_', '.'
+
+    $Latest = @{ URL64 = $url64; Version = $version }
+    return $Latest
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
