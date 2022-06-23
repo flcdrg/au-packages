d4ba52253630e108878e42c5adf664bba0c81ab7
David Gardiner <david@gardiner.net.au>
2022-06-23 09:35:16 +0930

    fix urls

--- oscar-cpap-analysis/tools/chocolateyinstall.ps1
+++ oscar-cpap-analysis/tools/chocolateyinstall.ps1
@@ -9,10 +9,10 @@ $packageArgs = @{
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   
   validExitCodes= @(0) #please insert other valid exit codes here
-  url           = "https://www.apneaboard.com/OSCAR/OSCAR-1.4.0-Win64.exe"
+  url           = "https://www.apneaboard.com/OSCAR/OSCAR-1.4.0-Win32.exe"
   checksum      = 'D8998239B5FBCC1E9A5A53CB6116368AD760F50C25ECC332196395772E038A98'
   checksumType  = 'sha256'
-  url64bit      = "https://www.apneaboard.com/OSCAR/OSCAR-1.4.0-Win32.exe"
+  url64bit      = "https://www.apneaboard.com/OSCAR/OSCAR-1.4.0-Win64.exe"
   checksum64    = '169497C717FDC50C9A541E8C407AF33E61F34A379D184C61C3483815BAB8CBEC'
   checksumType64= 'sha256'
   destination   = $toolsDir
