06de68886b1faebadab503fd6cad4243b3d4da9c
flcdrg <david@gardiner.net.au>
2022-09-27 06:04:27 +0000

    AU: 1 updated - resharper-clt.portable [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/54bb9f24eb4a2e24d7a602b475b1bf7a76c80d79

--- resharper-clt.portable/tools/chocolateyinstall.ps1
+++ resharper-clt.portable/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$url = 'https://download.jetbrains.com/resharper/dotUltimate.2022.2.3/JetBrains.ReSharper.CommandLineTools.2022.2.3.zip'
-$checksum = 'a2808bcdfdf066e6bf2d719ff31f0e75801b9b75d91f6777377ff69ffb04d80c'
+$url = 'https://download.jetbrains.com/resharper/dotUltimate.2022.3.EAP1/JetBrains.ReSharper.CommandLineTools.2022.3.EAP1.Checked.zip'
+$checksum = '3ea1b3a7e2fb95affe42ee56b51e3b8bd5e2366245ecca20a85e9dc40d447c9f'
 
 $installPath  = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
