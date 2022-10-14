519a6d32156223d35892ed20598dc7d60d2d5739
flcdrg <david@gardiner.net.au>
2022-10-14 00:08:20 +0000

    AU: 1 updated - vt-cli [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/5dfcd5200f43ae20f110380ce251eaef5144c295

--- vt-cli/tools/chocolateyinstall.ps1
+++ vt-cli/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/VirusTotal/vt-cli/releases/download/0.10.3/Windows32.zip'
-$url64      = 'https://github.com/VirusTotal/vt-cli/releases/download/0.10.3/Windows64.zip'
-$checksum   = 'e3a4900287501f1dab488abce6287c20aef27e6ddfd9961019a6b300ab50476e'
-$checksum64 = 'd4944611c446a9e67e7065b9bd2c04434240b60809e071ef9e153c89a2fa52c4'
+$url        = 'https://github.com/VirusTotal/vt-cli/releases/download/0.10.4/Windows32.zip'
+$url64      = 'https://github.com/VirusTotal/vt-cli/releases/download/0.10.4/Windows64.zip'
+$checksum   = 'aed67b753f5ede757d36aa5c92da5c45ef0388d8ae935dabeb5df90b1816e915'
+$checksum64 = '97ddbc169da118f6416cb4269b4c87fa44023c7b66e98cc60b7953bea45d0303'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
