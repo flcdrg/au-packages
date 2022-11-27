c7ab6f70bf3c14dfe070ff9a44f34e24678cd721
David Gardiner <david@gardiner.net.au>
2022-11-27 20:58:17 +1030

    sql server express 2022 advanced
    fixes #129

--- sql-server-express-adv/tools/chocolateyuninstall.ps1
+++ sql-server-express-adv/tools/chocolateyuninstall.ps1
@@ -1,7 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName= 'sql-server-express-adv'
-$silentArgs = ""
+$setupARPPath = ((Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft SQL Server SQL2022').UninstallString).Replace("`"", "")
 
-$setupPath = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft SQL Server SQLServer2016').UninstallString
-Uninstall-ChocolateyPackage "$packageName" "EXE" "$silentArgs" "$setupPath" -validExitCodes @(0, 3010)
+$setupPath = [IO.Path]::Combine([IO.Path]::GetDirectoryName([IO.Path]::GetDirectoryName($setupARPPath)), "setup.exe")
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file          = $setupPath
+  silentArgs    = "/Q /ACTION=Uninstall /INSTANCENAME=SQLEXPRESS /FEATURES=SQLENGINE,REPLICATION,ADVANCEDANALYTICS,FULLTEXT,BROWSER,WRITER /SUPPRESSPRIVACYSTATEMENTNOTICE=True"
+  validExitCodes= @(0, 3010, 1605, 1614, 1641)
+}
+
+Uninstall-ChocolateyPackage @packageArgs
