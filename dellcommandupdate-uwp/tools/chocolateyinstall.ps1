3feb16bed2800ee0244d4c6453d6893d03a77a99
David Gardiner <david@gardiner.net.au>
2022-11-30 20:55:33 +1030

    DCU UWP 4.7.1

--- dellcommandupdate-uwp/tools/chocolateyinstall.ps1
+++ dellcommandupdate-uwp/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://dl.dell.com/FOLDER08911558M/1/Dell-Command-Update-Windows-Universal-Application_DT6YC_WIN_4.6.0_A00.EXE'
-$checksum = '78137238c10c0251fc2aff2b818ad4676a86c2567d8ed3f1b93588c2619a147b'
+$url = 'https://dl.dell.com/FOLDER09268356M/1/Dell-Command-Update-Windows-Universal-Application_CJ0G9_WIN_4.7.1_A00.EXE'
+$checksum = 'f702bef5204302868cff794eb6d8820da02044b741479b27f425867a9491bef7'
 $checksumType = 'sha256'
 
 Write-Warning "Ensure that 'Dell Update for Windows 10' is not installed. Any errors from this can be ignored"
@@ -18,7 +18,7 @@ Uninstall-ChocolateyPackage @dellUpdateUninstallPackageArgs
  # the /factoryinstall parameter makes installer return success instead of '2' (which Chocolatey doesn't like)
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName  = 'Dell Command | Update for Windows 10'
+  softwareName  = 'Dell Command | Update for Windows Universal'
   fileType      = 'exe'
   silentArgs    = "/s /factoryinstall"
   validExitCodes= @(0)
