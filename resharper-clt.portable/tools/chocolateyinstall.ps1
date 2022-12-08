72a0424bdd59c602ecb7e0b8130669472f84d658
David Gardiner <david@gardiner.net.au>
2022-12-08 12:18:03 +1030

    Fix file

--- resharper-clt.portable/tools/chocolateyinstall.ps1
+++ resharper-clt.portable/tools/chocolateyinstall.ps1
@@ -1,8 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
 $url = 'https://download.jetbrains.com/resharper/dotUltimate.2022.3.EAP9/JetBrains.ReSharper.CommandLineTools.2022.3.EAP9.Checked.zip'
-$checksum = 'a49f9f516b8976f1f06e839b643532e46f5fdd38b0403c9fef92f61707a70c47 *JetBrains.ReSharper.CommandLineTools.2022.3.EAP9.Checked.zip
-'
+$checksum = 'a49f9f516b8976f1f06e839b643532e46f5fdd38b0403c9fef92f61707a70c47'
 
 $installPath  = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
