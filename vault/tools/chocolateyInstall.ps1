d696612296f623411bf88731793ec3345f67715a
David Gardiner <david@gardiner.net.au>
2022-12-17 15:49:13 +1030

    Add Vault

---
+++ vault/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageArgs = @{
+  PackageName         = "vault"
+  Url                 = "https://releases.hashicorp.com/vault/$($env:ChocolateyPackageVersion)/vault_$($env:ChocolateyPackageVersion)_windows_386.zip"
+  UnzipLocation       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  Url64               = "https://releases.hashicorp.com/vault/$($env:ChocolateyPackageVersion)/vault_$($env:ChocolateyPackageVersion)_windows_amd64.zip"
+  Checksum            = '749d285abbec5727c7a5b37ae5cac411c5c03f3685aae588890779749f89c8ee'
+  ChecksumType        = 'sha256'
+  Checksum64          = '0421531b5846a45a53547bc9a505de2b11d91d0a48fdb5aed1d77259720db5ab'
+  version             = $env:ChocolateyPackageVersion
+}
+
+Install-ChocolateyZipPackage @packageArgs
