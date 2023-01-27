6b65f2f2143e5c7587c47ae42167b4ee4303cddc
flcdrg <david@gardiner.net.au>
2023-01-27 00:10:30 +0000

    AU: 10 updated - dotCover dotMemory dotPeek dotTrace dotUltimate ReSharper resharper-clt ReSharperCpp resharper-ultimate-all teamCityAddin [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/f532c16f33c3209cc8448d194a820674215de225

--- dotMemory/tools/chocolateyinstall.ps1
+++ dotMemory/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$filename = 'JetBrains.dotUltimate.2023.1.EAP1.Checked.exe'
+$filename = 'JetBrains.dotUltimate.2022.3.2.exe'
 
 $platformPackageName = 'resharper-platform'
 
