68c95030ae00e88ff076feac3535cddae0c145e1
flcdrg <david@gardiner.net.au>
2023-02-09 00:09:00 +0000

    AU: 1 updated - azure-developer-cli [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/0c5960ca92a20b1fb0a9976a6c6b25abee4755b9

--- azure-developer-cli/tools/chocolateyinstall.ps1
+++ azure-developer-cli/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://github.com/Azure/azure-dev/releases/download/azure-dev-cli_0.5.0-beta.3/azd-windows-amd64.zip'
-$checksum      = 'bcdec21254094f15d0fb3912cbbd7a896f234056f5809b5276554de7587d34a1'
+$url        = 'https://github.com/Azure/azure-dev/releases/download/azure-dev-cli_0.6.0-beta.1/azd-windows-amd64.zip'
+$checksum      = '5828a8fc9786a7b223ed8be6cc079195b4213c8130f29b52fd3f2d9823716ba7'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
