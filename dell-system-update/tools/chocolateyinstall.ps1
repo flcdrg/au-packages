a55c094e20ae9ce4a43dab677df5c777a80e7f50
flcdrg <david@gardiner.net.au>
2023-03-25 00:10:12 +0000

    AU: 11 updated - dell-system-update dotCover dotMemory dotPeek dotTrace dotUltimate ReSharper resharper-clt resharper-ultimate-all ReSharperCpp teamCityAddin [skip ci] https://gist.github.com/flcdrg/7d00c69c2cde8309a8594abaa897a9a6/5f6dda61f6064d7859d86eb813d44ea84ba278fa

--- dell-system-update/tools/chocolateyinstall.ps1
+++ dell-system-update/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64 = 'https://downloads.dell.com/FOLDER08957950M/1/Systems-Management_Application_8D3X6_WN64_2.0.1.0_A00.EXE'
-$checksum64 = 'db1bf548909ca6e142a1af9196a011eb'
+$url64 = 'https://downloads.dell.com/FOLDER09663875M/1/Systems-Management_Application_RWVV0_WN64_2.0.2.0_A00.EXE'
+$checksum64 = 'b9282c78578c8a580d087ab31eae77c9'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
