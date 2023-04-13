e75d7c6bab5d83fddfc2f3263a176d894333c649
David Gardiner <david@gardiner.net.au>
2023-04-13 13:08:51 +0930

    AzDO Agent v2 and v3 support

--- azure-pipelines-agent/update.ps1
+++ azure-pipelines-agent/update.ps1
@@ -23,28 +23,35 @@ function global:au_GetLatest {
     }
 
     $releasesUrl = "https://api.github.com/repos/Microsoft/azure-pipelines-agent/releases"
-    $response = Invoke-RestMethod -Method Get -Uri "$releasesUrl/latest" -Headers $headers
-    
-    # Assume releases are named v1.2.3
-    if (!$response.name.StartsWith("v")) {
-        return @{}
-    }
+    $response = Invoke-RestMethod -Method Get -Uri $releasesUrl -Headers $headers
 
-    $version = $response.name.Substring(1)
-
-    $assets = Invoke-RestMethod -Method Get -Uri $response.assets_url -Headers $headers
-
-    $assetsJson = Invoke-RestMethod -Method Get -Uri $assets[0].browser_download_url -Headers $headers
-
-    # Note at some point we may want to offer the non-node installers as well, which use 'pipeline-agent' as a prefix
-    # See https://github.com/microsoft/azure-pipelines-agent/pull/3170 for more context
-    $asset32 = $assetsJson | Where-Object { $_.platform -eq "win-x86" -and $_.name.StartsWith("vsts-agent") }
-    $asset64 = $assetsJson | Where-Object { $_.platform -eq "win-x64" -and $_.name.StartsWith("vsts-agent") }
     $Latest = @{
-        Url32 = $asset32.downloadUrl
-        Url64 = $asset64.downloadUrl
-        Version = $version
+        Streams = [ordered] @{
+        }
     }
+
+    # Assume releases are named v1.2.3
+    foreach ($prefix in "v2", "v3") {
+        $release = $response | Where-Object { $_.name.StartsWith($prefix) -and -not $_.prerelease } | Select-Object -First 1
+
+        $version = $release.name.Substring(1)
+
+        $assets = Invoke-RestMethod -Method Get -Uri $release.assets_url -Headers $headers
+
+        $assetsJson = Invoke-RestMethod -Method Get -Uri $assets[0].browser_download_url -Headers $headers
+
+        # Note at some point we may want to offer the non-node installers as well, which use 'pipeline-agent' as a prefix
+        # See https://github.com/microsoft/azure-pipelines-agent/pull/3170 for more context
+        $asset32 = $assetsJson | Where-Object { $_.platform -eq "win-x86" -and $_.name.StartsWith("vsts-agent") }
+        $asset64 = $assetsJson | Where-Object { $_.platform -eq "win-x64" -and $_.name.StartsWith("vsts-agent") }
+
+        $Latest.Streams.Add($prefix, @{
+            Url32 = $asset32.downloadUrl
+            Url64 = $asset64.downloadUrl
+            Version = $version
+        })
+    }
+
     return $Latest
 }
 
