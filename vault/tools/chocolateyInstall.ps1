097d1cd1601942e5939837d909ddc398e426da83
flcdrg <david@gardiner.net.au>
2023-07-26 00:04:51 +0000

    AU: 3 updated - TeamCity-OpenJDK8 TeamCity-PreinstalledJRE vault [skip ci] https://gist.github.com/flcdrg/7d00c69c2cde8309a8594abaa897a9a6/be64544038176f358e3ab0f932da78021a9074a3

--- vault/tools/chocolateyInstall.ps1
+++ vault/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
   Url                 = "https://releases.hashicorp.com/vault/$($env:ChocolateyPackageVersion)/vault_$($env:ChocolateyPackageVersion)_windows_386.zip"
   UnzipLocation       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   Url64               = "https://releases.hashicorp.com/vault/$($env:ChocolateyPackageVersion)/vault_$($env:ChocolateyPackageVersion)_windows_amd64.zip"
-  Checksum            = 'fbc976d2825d5de4250c920c4a4e70d4457a0647093f6f2e90ae56e2360b46d9'
+  Checksum            = '3597bd74522d26ac7366e18694b68d26a292fdf9f17d63eb751846ca6db2be75'
   ChecksumType        = 'sha256'
-  Checksum64          = '96dee4c0363a38c9e6571369567dd8ab88753dda433599642ef635394b85c8ed'
+  Checksum64          = '880f4824f76f4ed36da8c5b877147fbd83c15d1277420c23a4f747fb810d7dd9'
   version             = $env:ChocolateyPackageVersion
 }
 
