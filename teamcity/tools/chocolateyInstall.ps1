6e0e161c83e8ec03956680ddc2aa9b6050a56cad
David Gardiner <david@gardiner.net.au>
2023-08-01 17:22:47 +0930

    Fixes

--- teamcity/tools/chocolateyInstall.ps1
+++ teamcity/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$filename = "TeamCity-$($env:ChocolateyPackageVersion).tar.gz"
+$filename = 'TeamCity-2023.05.2.tar.gz'
 $checksum = '5f30bd106d323ae333d3c7d35b5530a47a68c0fdbaaa7bca6e4d4b71b3fc5e38'
 
-$url = "https://download.jetbrains.com/teamcity/TeamCity-$($env:ChocolateyPackageVersion).tar.gz"
+$url = 'https://download.jetbrains.com/teamcity/TeamCity-2023.05.2.tar.gz'
 $packagePath = $(Split-Path -parent $toolsDir)
 $installPath = Join-Path $packagePath $filename
 
