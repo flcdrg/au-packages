54a2fb0af2aa06793c9df688bfca43095a138b3e
David Gardiner <david@gardiner.net.au>
2023-08-03 22:31:14 +0930

    More TeamCity fixes

--- TeamCity-PreinstalledJRE/update.ps1
+++ TeamCity-PreinstalledJRE/update.ps1
@@ -4,6 +4,10 @@ import-module au
 
 function global:au_SearchReplace {
     @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]version\s*=\s*)('.*')"      = "`$1'$($Latest.Version)'"
+        }
+
         #       <dependency id="teamcity" version="[2019.1]" />
         "$($Latest.PackageName).nuspec" = @{
             "(\<dependency id=`"teamcity`" version=`"\[).*?(\]`" /\>)" = "`${1}$($Latest.Version)`$2"
