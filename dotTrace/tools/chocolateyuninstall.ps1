42e0bd5a18eeccee36bc927e512b06320d1347fe
flcdrg <david@gardiner.net.au>
2023-09-27 00:09:09 +0000

    AU: 11 updated - dotCover dotMemory dotPeek dotTrace dotUltimate ReSharper resharper-clt resharper-ultimate-all ReSharperCpp teamCityAddin vault [skip ci] https://gist.github.com/flcdrg/7d00c69c2cde8309a8594abaa897a9a6/4a24f3fbc65df7d76f7770e805707a85eaeb2693

--- dotTrace/tools/chocolateyuninstall.ps1
+++ dotTrace/tools/chocolateyuninstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$filename = 'JetBrains.dotUltimate.2023.3.EAP1.Checked.exe'
+$filename = 'JetBrains.dotUltimate.2023.2.2.exe'
 
 $platformPackageName = 'resharper-platform'
 
--- vault/tools/chocolateyInstall.ps1
+++ vault/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
   Url                 = "https://releases.hashicorp.com/vault/$($env:ChocolateyPackageVersion)/vault_$($env:ChocolateyPackageVersion)_windows_386.zip"
   UnzipLocation       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   Url64               = "https://releases.hashicorp.com/vault/$($env:ChocolateyPackageVersion)/vault_$($env:ChocolateyPackageVersion)_windows_amd64.zip"
-  Checksum            = '9000d1023c225d4c998ebdedbc0ca0051b2cbec7b37b4594a07822cbe8441e6b'
+  Checksum            = 'ad94fd7415b9eab7eab1dde28dbab4e102b757959eb32f1bae55362106e59a80'
   ChecksumType        = 'sha256'
-  Checksum64          = '697904a3359665d132c43424d94a812c0b95b7a2254f3ef05445b508b278f234'
+  Checksum64          = '69b4a6fab1a9bdf3a619a4cf358dfb824e5d1facb2f5f039a94aa12f336342a6'
   version             = $env:ChocolateyPackageVersion
 }
 
