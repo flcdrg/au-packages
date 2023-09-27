9490cd4a5198f21da26391de86f29df5857f4750
flcdrg <david@gardiner.net.au>
2023-09-27 00:07:15 +0000

    AU: 2 updated - resharper-clt.portable resharper-platform [skip ci] https://gist.github.com/flcdrg/7d00c69c2cde8309a8594abaa897a9a6/b3730a0f1931af8ffa832a58968604d3f44d3b8d

--- resharper-clt.portable/tools/chocolateyinstall.ps1
+++ resharper-clt.portable/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$url = 'https://download.jetbrains.com/resharper/dotUltimate.2023.3.EAP1/JetBrains.ReSharper.CommandLineTools.2023.3.EAP1.Checked.zip'
-$checksum = '8a903206ff9a163a4aa2d378babc8c64c68afac8aadb8ef43969046b26759420'
+$url = 'https://download.jetbrains.com/resharper/dotUltimate.2023.2.2/JetBrains.ReSharper.CommandLineTools.2023.2.2.zip'
+$checksum = '65bd0749ad80d139cbcba5e89cb49cae04a685d58cbb0e535628674cfe90a7fa'
 
 $installPath  = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
--- resharper-platform/tools/chocolateyinstall.ps1
+++ resharper-platform/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$filename = 'JetBrains.dotUltimate.2023.3.EAP1.Checked.exe'
-$checksum = 'e8f51ee276741f2e2a9cccbeb6ffe3d64648a5735f344ae11040ae2d4320afbf'
+$filename = 'JetBrains.dotUltimate.2023.2.2.exe'
+$checksum = '374c9a84115042ebca819d46e07e168c1a2d4b1055216a82ff3b2fc9895c23d2'
 
-$url = 'https://download.jetbrains.com/resharper/dotUltimate.2023.3.EAP1/JetBrains.dotUltimate.2023.3.EAP1.Checked.exe'
+$url = 'https://download.jetbrains.com/resharper/dotUltimate.2023.2.2/JetBrains.dotUltimate.2023.2.2.exe'
 $packagePath = $(Split-Path -parent $toolsDir)
 $installPath = Join-Path $packagePath $filename
 
