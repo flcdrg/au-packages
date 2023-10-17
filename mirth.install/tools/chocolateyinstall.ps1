85b0b534ee210dd45d161ff2153f16073da057c3
flcdrg <david@gardiner.net.au>
2023-10-17 00:05:20 +0000

    AU: 1 updated - mirth.install [skip ci] https://gist.github.com/flcdrg/7d00c69c2cde8309a8594abaa897a9a6/615594fcf84d775499ed5501e679fee13fc38c0c

--- mirth.install/tools/chocolateyinstall.ps1
+++ mirth.install/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://s3.amazonaws.com/downloads.mirthcorp.com/connect/4.4.0.b2948/mirthconnect-4.4.0.b2948-windows-x32.exe'
-$url64      = 'https://s3.amazonaws.com/downloads.mirthcorp.com/connect/4.4.0.b2948/mirthconnect-4.4.0.b2948-windows-x64.exe'
-$checksum   = 'e94677be0714a67f1cabd470a1b83276a66d7150747c51203947686968a9a8e1'
-$checksum64 = '34223057e0f01a2ce100197e0c3d3d908723df882a2196f683610daa00eef8c0'
+$url        = 'https://s3.amazonaws.com/downloads.mirthcorp.com/connect/4.4.1.b310/mirthconnect-4.4.1.b310-windows-x32.exe'
+$url64      = 'https://s3.amazonaws.com/downloads.mirthcorp.com/connect/4.4.1.b310/mirthconnect-4.4.1.b310-windows-x64.exe'
+$checksum   = '385da84be8e14c0b07508e82f28517283020fc8e316e0d642ec9a2e3fc141826'
+$checksum64 = '218a40358c48952a958ead0121637ef38302fed961d67d530139d7a55335ca31'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
