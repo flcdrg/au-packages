8d8972ccc2c1c2eac577dcf9a64081f366392b7c
flcdrg <david@gardiner.net.au>
2023-10-26 00:06:56 +0000

    AU: 1 updated - vault [skip ci] https://gist.github.com/flcdrg/7d00c69c2cde8309a8594abaa897a9a6/4767b5d9b34ab80d3fe7507a8136eacdd9b614b3

--- vault/tools/chocolateyInstall.ps1
+++ vault/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
   Url                 = "https://releases.hashicorp.com/vault/$($env:ChocolateyPackageVersion)/vault_$($env:ChocolateyPackageVersion)_windows_386.zip"
   UnzipLocation       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   Url64               = "https://releases.hashicorp.com/vault/$($env:ChocolateyPackageVersion)/vault_$($env:ChocolateyPackageVersion)_windows_amd64.zip"
-  Checksum            = 'ad94fd7415b9eab7eab1dde28dbab4e102b757959eb32f1bae55362106e59a80'
+  Checksum            = 'a6af2a87378212dcdfdc1c0ef3e67e63831c89efff722a1fa4db824c1f7b5ab6'
   ChecksumType        = 'sha256'
-  Checksum64          = '69b4a6fab1a9bdf3a619a4cf358dfb824e5d1facb2f5f039a94aa12f336342a6'
+  Checksum64          = '2b95c8dfab77c9915b4471366e3fc348a41e96d60072cec14b30ae9860c8dd1f'
   version             = $env:ChocolateyPackageVersion
 }
 
