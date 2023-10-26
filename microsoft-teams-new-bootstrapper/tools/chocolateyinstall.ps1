a441d3dd9b117e3e3c34fc6b06f070a382718ba6
David Gardiner <david@gardiner.net.au>
2023-10-26 21:54:24 +1030

    Add Teams new client bootstrapper

---
+++ microsoft-teams-new-bootstrapper/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$minimumOsVersion = "10.0.19041" # 20H1
+$osVersion = (Get-CimInstance Win32_OperatingSystem).Version
+if ([Version] $osVersion -lt [version] $minimumOsVersion) {
+  Write-Error "Microsoft Teams New Client requires a minimum of Windows 10 20H1 version $minimumOsVersion. You have $osVersion"
+}
+
+$downloadPath = Join-Path $toolsDir "teamsbootstrapper.exe"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'microsoft-teams-new-bootstrapper*'
+  fileType      = 'exe'
+  
+  url           = "https://statics.teams.cdn.office.net/production-teamsprovision/lkg/teamsbootstrapper.exe"
+  checksum      = '9BA82898539EEDF786346C17041A072C7002541504BAF4B1612F2D097EB9CC81'
+  checksumType  = 'sha256'
+  FileFullPath  = $downloadPath
+}
+
+Get-ChocolateyWebFile @packageArgs
+
+& $downloadPath -p
