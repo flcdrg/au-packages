bf6cb85a7c122a4d1efe8ad5fd9b363c2c6e64ce
flcdrg <david@gardiner.net.au>
2023-11-29 22:25:58 +0000

    AU: 11 updated - dotCover dotMemory dotPeek dotTrace dotUltimate ReSharper resharper-clt resharper-ultimate-all ReSharperCpp teamCityAddin terrascan [skip ci] https://gist.github.com/flcdrg/7d00c69c2cde8309a8594abaa897a9a6/134c62caab3452da1d81b69c5303a233d407283f

--- dotCover/tools/chocolateyuninstall.ps1
+++ dotCover/tools/chocolateyuninstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$filename = 'JetBrains.dotUltimate.2023.3.EAP8.Checked.exe'
+$filename = 'JetBrains.dotUltimate.2023.3.RC1.Checked.exe'
 
 $platformPackageName = 'resharper-platform'
 
--- terrascan/tools/chocolateyinstall.ps1
+++ terrascan/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'terrascan*'
   
-  url           = 'https://github.com/tenable/terrascan/releases/download/v1.18.3/terrascan_1.18.3_Windows_i386.zip'
-  checksum      = 'fbe13d4a7d0bb3f8cf20c1f2ede2eacded83b43b6d5a41c916d3474d8580e0c3'
+  url           = 'https://github.com/tenable/terrascan/releases/download/v1.18.4/terrascan_1.18.4_Windows_i386.zip'
+  checksum      = '59981e2c64f3e2a174201105aa2c6f92757542eb1ef0b316a63ebfcf1e655899'
   checksumType  = 'sha256'
-  url64bit      = 'https://github.com/tenable/terrascan/releases/download/v1.18.3/terrascan_1.18.3_Windows_x86_64.zip'
-  checksum64    = '105338ef3082db0b57faeee026443264554c1a46da5f81ee70daf7bdeebdf8a4'
+  url64bit      = 'https://github.com/tenable/terrascan/releases/download/v1.18.4/terrascan_1.18.4_Windows_x86_64.zip'
+  checksum64    = '6c03d4605de71779eb28123c46d7909c468498f6bde7a0615dc35b31717266c0'
   checksumType64= 'sha256'
   destination   = $toolsDir
 }
