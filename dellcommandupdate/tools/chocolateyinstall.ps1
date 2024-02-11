9c13eeb618c70042e384a38124c4dc68a9751398
David Gardiner <david@gardiner.net.au>
2024-02-12 09:43:22 +1030

    Set user agent for dell download

--- dellcommandupdate/tools/chocolateyinstall.ps1
+++ dellcommandupdate/tools/chocolateyinstall.ps1
@@ -16,6 +16,7 @@ $packageArgs = @{
   checksum      = $checksum
   checksumType  = $checksumType
   destination   = $toolsDir
+  options       = @{ Headers = @{ 'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0' } }
 }
 
 Install-ChocolateyPackage @packageArgs 
