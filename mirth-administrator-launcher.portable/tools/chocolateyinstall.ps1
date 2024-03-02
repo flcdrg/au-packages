ae913e09ef3c1eaeba84ae71be023dc3bbb6116c
David Gardiner <david@gardiner.net.au>
2024-03-02 14:29:51 +1030

    Mirth updates

--- mirth-administrator-launcher.portable/tools/chocolateyinstall.ps1
+++ mirth-administrator-launcher.portable/tools/chocolateyinstall.ps1
@@ -1,17 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-#Based on NO DETECTION IN PRO
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'zip'
   
-  validExitCodes= @(0) #please insert other valid exit codes here
-  url           = "https://s3.amazonaws.com/downloads.mirthcorp.com/connect-client-launcher/mirth-administrator-launcher-1.1.0-windows.zip"  #download URL, HTTPS preferrred
-  checksum      = '0CC7C85C46AD0D73659A8F759824825DE4FAF62C60A04957410937568AE609EE'
+  validExitCodes= @(0)
+  url           = "https://s3.us-east-1.amazonaws.com/downloads.mirthcorp.com/connect-client-launcher/mirth-administrator-launcher-1.4.1-windows-x32.zip"
+  checksum      = '59A7DF44694F91943977D35AE0F3AF483A13F924A3EA77EF663B8D7343DED4FD'
   checksumType  = 'sha256'
-  url64bit      = "https://s3.amazonaws.com/downloads.mirthcorp.com/connect-client-launcher/mirth-administrator-launcher-1.1.0-windows-x64.zip"   # 64bit URL here (HTTPS preferred) or remove - if installer contains both architectures (very rare), use $url
-  checksum64    = 'AD24DBE341E77229C7F644378992FB2D33FE48A760730DEBDB6914DEB2977D04'
+  url64bit      = "https://s3.us-east-1.amazonaws.com/downloads.mirthcorp.com/connect-client-launcher/mirth-administrator-launcher-1.4.1-windows-x64.zip"
+  checksum64    = '23CCC489600CCEB321269614E910D46CE7574E33572DB73EBB12294B0E2E0B43'
   checksumType64= 'sha256'
   destination   = $toolsDir
 }
