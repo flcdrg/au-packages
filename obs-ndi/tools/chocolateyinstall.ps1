42bb4f3a0930dd06acb958d83328f4c2b4142a1c
David Gardiner <david@gardiner.net.au>
2024-03-03 17:27:49 +1030

    NDI Runtime update

--- obs-ndi/tools/chocolateyinstall.ps1
+++ obs-ndi/tools/chocolateyinstall.ps1
@@ -1,6 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+$ndiChecksum = 'BC666D409FA6B2D81FF8650A5B92481742F5E895E4994A1A414BAC423CD8448C'
+
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
 $url      = 'https://github.com/obs-ndi/obs-ndi/releases/download/4.13.0/obs-ndi-4.13.0-windows-x64-Installer.exe'
 $checksum = '101DC8A7E4E84DCCF2F89D3A56EEB4C18B9FE83EFF54C3A23F2D104B3545E0C5'
@@ -12,7 +14,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = 'http://ndi.link/NDIRedistV5'
   softwareName  = 'NDI 5 Runtime'
-  checksum      = 'C255860560C86EDFFD77697413EE0ABB33847809E811449F790D1D00A0771CBF'
+  checksum      = $ndiChecksum
   checksumType  = 'sha256'
   silentArgs    = '/VERYSILENT /LOG /NORESTART /SUPPRESSMSGBOXES'
   validExitCodes= @(0)
