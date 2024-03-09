b3dd796ee15dbd6b5008d2166c6d3e66e0db4de6
David Gardiner <david@gardiner.net.au>
2024-03-09 11:53:46 +1030

    Patch dellcommandupdate

--- dellcommandupdate/tools/chocolateyinstall.ps1
+++ dellcommandupdate/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName  = 'dellcommandupdate'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://dl.dell.com/FOLDER11201514M/1/Dell-Command-Update-Application_4R78G_WIN_5.2.0_A00.EXE'
+$url = 'https://downloads.dell.com/FOLDER11201514M/1/Dell-Command-Update-Application_4R78G_WIN_5.2.0_A00.EXE'
 $checksum = '2f7f8c69ee80a207061dbf7579d7a5d252d8eb4434de69610d1355a125fcba45'
 $checksumType = 'sha256'
 
