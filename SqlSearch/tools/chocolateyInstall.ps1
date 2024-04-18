829d9a89422f2d7ef01d0c1e79698908d1772923
David Gardiner <david@gardiner.net.au>
2024-04-18 15:42:30 +0930

    SqlSearch 2024.04.10

--- SqlSearch/tools/chocolateyInstall.ps1
+++ SqlSearch/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$checksum = '21C63FBAE2660CD1D44B27C40FC46AB0529CC497636C2222C880C20480A0AFDC'
-$url = 'https://download.red-gate.com/installers/SQL_Search/2023-09-14/SQL_Search.exe'
+$checksum = '4BFC387AB7CC32B2245AB0CC90B0F9BA386DA9385F9C1FAA7E1AB0E815AC2FD8'
+$url = 'https://download.red-gate.com/installers/SQLSearch/2024-04-10/SQLSearch.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
