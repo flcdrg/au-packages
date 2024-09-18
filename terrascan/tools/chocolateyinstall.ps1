57fc3c90079cedce75bab78e95f9da2e5e202a73
David Gardiner <david@gardiner.net.au>
2024-09-18 09:08:49 +0000

    AU-dotnet: 1 updated [skip ci]

--- terrascan/tools/chocolateyinstall.ps1
+++ terrascan/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'terrascan*'
   
-  url           = 'https://github.com/tenable/terrascan/releases/download/v1.19.8/terrascan_1.19.8_Windows_i386.zip'
-  checksum      = '4600db5dd0c8e15f6b9e45f64019541d0872d1391b963e03484768f72ca27e81'
+  url           = 'https://github.com/tenable/terrascan/releases/download/v1.19.9/terrascan_1.19.9_Windows_i386.zip'
+  checksum      = 'aa546c8b45c9154a7ff1b000d5164558a1bbf77cff00ae4f2eb8991c4c04cad2'
   checksumType  = 'sha256'
-  url64bit      = 'https://github.com/tenable/terrascan/releases/download/v1.19.8/terrascan_1.19.8_Windows_x86_64.zip'
-  checksum64    = '0f03b568a713d4f49e0652289e671649f65c78f042f44a882b9fec1b03fa46df'
+  url64bit      = 'https://github.com/tenable/terrascan/releases/download/v1.19.9/terrascan_1.19.9_Windows_x86_64.zip'
+  checksum64    = '2585e0ae7121bfa28a471175ec3cabe810680fcbeb064eefab93a8c6c475b094'
   checksumType64= 'sha256'
   destination   = $toolsDir
 }
