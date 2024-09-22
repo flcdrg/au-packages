4672907168aebd0668bed3bb6ec9bb69e74c1f96
David Gardiner <david@gardiner.net.au>
2024-09-23 08:45:49 +0930

    Update dotMemory Console and Unit

--- dotMemory-Console.portable/tools/chocolateyinstall.ps1
+++ dotMemory-Console.portable/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$url = 'https://download-cdn.jetbrains.com/resharper/dotUltimate.2022.1.2/JetBrains.dotMemory.Console.windows-x64.2022.1.2.zip'
-$checksum = '4be497788ff221f115ab9280190e4286a343eabb0d4f68f3a4e169ba8c4da6f4'
+$url = 'https://download-cdn.jetbrains.com/resharper/dotUltimate.2024.2.5/JetBrains.dotMemory.Console.windows-x64.2024.2.5.zip'
+$checksum = '8d02437a72d14ebcf099b6cca54e4c8a650e7a3cd41a401cf42d50dc1d683dc2'
 
 $installPath  = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
