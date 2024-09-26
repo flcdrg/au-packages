980823ac8a746e6b56e899f8a8c24c9384b9f154
David Gardiner <david@gardiner.net.au>
2024-09-26 00:09:21 +0000

    AU-dotnet: 2 updated [skip ci]

--- sql-server-2022-cumulative-update/tools/chocolateyinstall.ps1
+++ sql-server-2022-cumulative-update/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://download.microsoft.com/download/9/6/8/96819b0c-c8fb-4b44-91b5-c97015bbda9f/SQLServer2022-KB5038325-x64.exe'
-$checksum   = '954287b6d8e64612e93e9137379002d49a401caf1e37b5697d340f127177aeda'
-$softwareName = 'Hotfix 4135 for SQL Server 2022*(KB5038325)*'
+$url        = 'https://download.microsoft.com/download/9/6/8/96819b0c-c8fb-4b44-91b5-c97015bbda9f/SQLServer2022-KB5041321-x64.exe'
+$checksum   = '758ee805faa130028085114fb2bce215765ee10c9648ddb80dab40fd8d6ae51a'
+$softwareName = 'Hotfix 4145 for SQL Server 2022*(KB5041321)*'
 
 [bool] $runningAU = (Test-Path Function:\au_GetLatest)
 
--- vault/tools/chocolateyInstall.ps1
+++ vault/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
   Url                 = "https://releases.hashicorp.com/vault/$($env:ChocolateyPackageVersion)/vault_$($env:ChocolateyPackageVersion)_windows_386.zip"
   UnzipLocation       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   Url64               = "https://releases.hashicorp.com/vault/$($env:ChocolateyPackageVersion)/vault_$($env:ChocolateyPackageVersion)_windows_amd64.zip"
-  Checksum            = '3bc65b69b85359bfd93c68df16b06af54f71bb1ab50d2c128391b7f9737e2f44'
+  Checksum            = '9498e5905fdba6d25211634651bbcbd871f9f241a0108f353ed917ae5b7f8ba0'
   ChecksumType        = 'sha256'
-  Checksum64          = '70fce89fbae13c874bfa668ff099abc617f3eea470b49bbd68dee7ebd358379d'
+  Checksum64          = '4b66a86e6a6033103a102bce5a3aa660c81c244cb3841a37374f71f069f6d191'
   version             = $env:ChocolateyPackageVersion
 }
 
