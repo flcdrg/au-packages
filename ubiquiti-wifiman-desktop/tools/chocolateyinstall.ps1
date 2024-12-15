b857bfcea374f7cebb2f4f85c05723aee07760be
David Gardiner <david@gardiner.net.au>
2024-12-15 15:33:50 +1030

    Ubiquiti Wifiman Desktop (updates)

--- ubiquiti-wifiman-desktop/tools/chocolateyinstall.ps1
+++ ubiquiti-wifiman-desktop/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName  = 'ubiquiti-wifiman-desktop*'
+  softwareName  = 'WiFiman Desktop'
   fileType      = 'exe'
-  silentArgs    = "/s"
+  silentArgs    = "/S"
   validExitCodes= @(0)
   url64bit      = "https://desktop.wifiman.com/wifiman-desktop-1.1.0-amd64.exe"
   checksum64    = 'CB22CC3EBD21BADC64524452B5FD54A19AF5010EFB52D1EB0EAF319A3D3D961F'
