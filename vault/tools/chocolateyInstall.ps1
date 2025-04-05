292454aefec8d518a3a7e8853ced5204845a8c19
David Gardiner <david@gardiner.net.au>
2025-04-05 00:07:02 +0000

    AU-dotnet: 2 updated [skip ci]

--- vault/tools/chocolateyInstall.ps1
+++ vault/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
   Url                 = "https://releases.hashicorp.com/vault/$($env:ChocolateyPackageVersion)/vault_$($env:ChocolateyPackageVersion)_windows_386.zip"
   UnzipLocation       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   Url64               = "https://releases.hashicorp.com/vault/$($env:ChocolateyPackageVersion)/vault_$($env:ChocolateyPackageVersion)_windows_amd64.zip"
-  Checksum            = 'f1d03346fef1d726a268f4063bfa41b48a356c4d4c6f6cae6cf234049b81cf79'
+  Checksum            = '0ad7019acef4e3e0e275f370a156d9da1c3c30467be1ea8b99af7a53af955abd'
   ChecksumType        = 'sha256'
-  Checksum64          = '4a288f258f9e55caaef494d4bf4d20d885632e49d9883c8dc4f059e64d9aa00d'
+  Checksum64          = '5701fdef2a22184217cf90d7f0a84634ae6c1a384f38bab451eb7911f9ed3188'
   version             = $env:ChocolateyPackageVersion
 }
 
