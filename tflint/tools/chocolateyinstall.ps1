a8131c5fdeb70e32e59946847f83ad1684a3066e
David Gardiner <david@gardiner.net.au>
2025-05-04 00:07:04 +0000

    AU-dotnet: 1 updated [skip ci]

--- tflint/tools/chocolateyinstall.ps1
+++ tflint/tools/chocolateyinstall.ps1
@@ -5,13 +5,13 @@ $packageArgs = @{
     packageName    = 'tflint'
 
     # 32-bit
-    url            = 'https://github.com/terraform-linters/tflint/releases/download/v0.56.0/tflint_windows_386.zip'
-    checksum       = 'f46e06f21d94c94444b266ba7e66b40af418caadfe1d7d633aa06f91a4150c1c'
+    url            = 'https://github.com/terraform-linters/tflint/releases/download/v0.57.0/tflint_windows_386.zip'
+    checksum       = '634b363a6e9996e05183e5615ae1bb1ed3c41af42ad778554f798f8fc8bb309a'
     checksumType   = 'sha256'
 
     # 64-bit
-    url64bit       = 'https://github.com/terraform-linters/tflint/releases/download/v0.56.0/tflint_windows_amd64.zip'
-    checksum64     = '8b1aeea099fb6bfae5cfc3437dc055909287ebef5212bbfcc6c3e211a515fef0'
+    url64bit       = 'https://github.com/terraform-linters/tflint/releases/download/v0.57.0/tflint_windows_amd64.zip'
+    checksum64     = 'ed0ef8236e632d78e951aa5335d19cbd591f746267e8b60655be4b29f68e4d2c'
     checksumType64 = $checksumType
 
     # misc
