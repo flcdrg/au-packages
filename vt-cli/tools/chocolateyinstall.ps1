d3292163597fda87d77297d8f73e5fda9e8657b8
David Gardiner <david@gardiner.net.au>
2025-06-07 00:08:01 +0000

    AU-dotnet: 1 updated [skip ci]

--- vt-cli/tools/chocolateyinstall.ps1
+++ vt-cli/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/VirusTotal/vt-cli/releases/download/1.0.1/Windows32.zip'
-$url64      = 'https://github.com/VirusTotal/vt-cli/releases/download/1.0.1/Windows64.zip'
-$checksum   = '45c2aeb98fae8664f2b25b8db8322e87de0578092581d6e941cfb2742e484368'
-$checksum64 = 'dd08ed6653c2e22fa085e651e624a2554535dcfa1483b4f3610698f4295bb8c9'
+$url        = 'https://github.com/VirusTotal/vt-cli/releases/download/1.1.1/Windows32.zip'
+$url64      = 'https://github.com/VirusTotal/vt-cli/releases/download/1.1.1/Windows64.zip'
+$checksum   = '5bcd4e85e9925f80bd1e02e7eaab50ed37c1999cfe59d144ab7c5b5a1a534b4e'
+$checksum64 = '469b7e7f4d5ccfce12a0576a82822ecd431e852052517af28acf67d93609fd14'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
