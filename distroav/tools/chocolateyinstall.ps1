86756c9a075151cbefd05e8658f024ae34f2f173
David Gardiner <david@gardiner.net.au>
2025-06-17 09:39:04 +0930

    Add missing checksum

--- distroav/tools/chocolateyinstall.ps1
+++ distroav/tools/chocolateyinstall.ps1
@@ -1,22 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
-$url      = 'https://github.com/DistroAV/DistroAV/releases/download/6.1.1/distroav-6.1.1-windows-x64-Installer.exe'
-$checksum = ''
-
+$url = 'https://github.com/DistroAV/DistroAV/releases/download/6.1.1/distroav-6.1.1-windows-x64-Installer.exe'
+$checksum = 'E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855'
 
 # Now install obs-ndi
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
-  softwareName  = 'obs-ndi*'
-  checksum      = $checksum
-  checksumType  = 'sha256'
-  silentArgs    = '/VERYSILENT /LOG /NORESTART /SUPPRESSMSGBOXES'
-  validExitCodes= @(0)
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url
+  softwareName   = 'obs-ndi*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  silentArgs     = '/VERYSILENT /LOG /NORESTART /SUPPRESSMSGBOXES'
+  validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
