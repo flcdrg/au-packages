ff1605a4c24ddce5779799f95784b0630380a5c4
David Gardiner <david@gardiner.net.au>
2025-06-22 16:21:42 +0930

    Update Dell Command Update UWP
    Fixes #207

--- dellcommandupdate-uwp/tools/chocolateyinstall.ps1
+++ dellcommandupdate-uwp/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://downloads.dell.com/FOLDER12925773M/1/Dell-Command-Update-Windows-Universal-Application_P4DJW_WIN64_5.5.0_A00.EXE'
-$checksum = '7d46abc002046d15d005a774123973f232080a51bc13f9cb2e3709cc1bd91e73'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://dl.dell.com/FOLDER13257249M/1/Dell-Command-Update-Windows-Universal-Application_66WNX_WIN64_5.5.0_A00.EXE'
+$checksum = '68551c7f2bb6c77a18a293db98064f9a6d4067bfe3887b131d1aa563b309b138'
 $checksumType = 'sha256'
 
 Write-Warning "Ensure that 'Dell Update for Windows 10' is not installed. Any errors from this can be ignored"
 # Package version 4.3.0 mistakenly installed "Dell Update" instead of "Dell Command Update", so we need to ensure that is uninstalled first
-$dellUpdateUninstallPackageArgs  = @{
+$dellUpdateUninstallPackageArgs = @{
   packageName    = 'Dell Update for Windows 10'
   fileType       = 'msi'
   silentArgs     = "{41D2D254-D869-4CD8-B440-5DF49083C4BA} /qn /norestart"
@@ -15,17 +15,29 @@ $dellUpdateUninstallPackageArgs  = @{
 }
 Uninstall-ChocolateyPackage @dellUpdateUninstallPackageArgs
 
- # the /factoryinstall parameter makes installer return success instead of '2' (which Chocolatey doesn't like)
+# the /factoryinstall parameter makes installer return success instead of '2' (which Chocolatey doesn't like)
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'Dell Command | Update for Windows Universal'
-  fileType      = 'exe'
-  silentArgs    = "/s /factoryinstall"
-  validExitCodes= @(0)
-  url           = $url
-  checksum      = $checksum
-  checksumType  = $checksumType
-  destination   = $toolsDir
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'Dell Command | Update for Windows Universal'
+  File           = [IO.Path]::Combine($toolsDir, [IO.Path]::GetFileName($url))
+  fileType       = 'exe'
+  silentArgs     = "/s /factoryinstall"
+  validExitCodes = @(0)
+  url            = $url
+  checksum       = $checksum
+  checksumType   = $checksumType
+  UserAgent      = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
 
-Install-ChocolateyPackage @packageArgs 
+# Install-ChocolateyPackage @packageArgs
+
+# As Dell have decommissioned downloads.dell.com we're forced to use dl.dell.com instead.
+# BUT Dell check user-agents on that URL (and doesn't like Chocolatey's default one), 
+# and because of https://github.com/chocolatey/choco/issues/3191
+# we can't override the user agent when calling Install-ChocolateyPackage.
+# So we resort to doing the download and checksum verification ourselves.
+Get-WebFile @packageArgs
+
+Get-ChecksumValid @packageArgs
+
+Install-ChocolateyInstallPackage @packageArgs
