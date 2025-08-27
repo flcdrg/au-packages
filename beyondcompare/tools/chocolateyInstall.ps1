3c14151737416602d6480b26e37366ee0bac432a
David Gardiner <david@gardiner.net.au>
2025-08-27 00:06:10 +0000

    AU-dotnet: 1 updated [skip ci]

--- beyondcompare/tools/chocolateyInstall.ps1
+++ beyondcompare/tools/chocolateyInstall.ps1
@@ -14,7 +14,7 @@ $german = @(3079,1031,5127,4103,2055)
 $french = @(2060,11276,3084,9228,12300,1036,5132,13324,6156,14348,10252,4108,7180)
 $japanese = @(17, 1041)
 $chinese = @(2052)
-$version = '5.1.3.31238'
+$version = '5.1.4.31268'
 
 # Install for all users by default
 if ($pp["CurrentUser"]) {
@@ -32,11 +32,11 @@ $packageArgs = @{
   checksumType  = 'sha256'
 }
 
-$checksumde = '2f4e694bb6dccd93049b3a6c48eca3fd9632962fb1a8cd00b45a2b1feef3c18b'
-$checksumfr = 'ff8a607cb2077a1d59105072fd3ae1585c2fec7ec47ae8f40827286f1a74a500'
-$checksumjp = 'a36957f10e0f5e4ca0e5895a6491f2d60fc62f50a8b5efbbe581cda0ce6f5519'
-$checksumzh = '57826b6602b2f6bf1b7c83bbe139cffd8a5c9c128cd39deca830930c252f346a'
-$checksum = '68b13054d330287490b472708d3f2ef94562bcc38e3de15174121672781b5d3f'
+$checksumde = '5fb4c8c67306d22ecc9344c31da90315eb61751633bf1a6d965dee1fca5e65b0'
+$checksumfr = '37c8bcdc32de5f7ec6e97e3a58f2392009421404c4c2d05715df4ac4469947f7'
+$checksumjp = 'c3064d888d3c2ddfc55d21b01ad253bcf8bbb53603c217a207876bf088d24d82'
+$checksumzh = 'cb56efc0fea5736c1446921a27148df123390b5070666e6a3a780ccecd720092'
+$checksum = '0339e29bc42ad0e0bd549d2fd20ac2e655af468a762730764ad06fe4b63236fb'
 
 if ($german -contains $LCID)
 {
