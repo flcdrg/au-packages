1d8a0df78b9606bd48e5e19c4853da0f89ee0a10
David Gardiner <david@gardiner.net.au>
2025-09-09 08:25:15 +0930

    Deprecate TeamCity Addin

--- teamCityAddin/tools/chocolateyuninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$platformPackageName = 'resharper-platform'
-$packageName = 'teamCityAddin'
-$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
-$filename = 'JetBrains.dotUltimate.2025.2.1.exe'
-
-$installPath = Join-Path  (Join-Path $commonPath $platformPackageName) $filename
-Uninstall-ChocolateyPackage $packageName 'exe' '/Silent=True /SpecificProductNamesToRemove=teamCityAddin /VsVersion=*' $installPath
