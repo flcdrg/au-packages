b15125620f46aa709d35fc1323cfa89b0fe6212f
David Gardiner <david@gardinerfamily.au>
2025-12-15 22:27:29 +1030

    Set useragent for Dell packages

--- dellcommandupdate-uwp/update.ps1
+++ dellcommandupdate-uwp/update.ps1
@@ -220,4 +220,6 @@ function global:au_AfterUpdate ($Package) {
     VirusTotal_AfterUpdate $Package
 }
 
+$global:au_UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+
 update -ChecksumFor none
