1f26002edde8fa5265e98009db7c64dff5baaf43
David Gardiner <david@gardinerfamily.au>
2026-01-13 00:04:27 +0000

    AU-dotnet: 1 updated [skip ci]

--- rode-connect/tools/chocolateyinstall.ps1
+++ rode-connect/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$checksum = 'AF4777C9DB263EF77BCF520C71A8B4FEE8E90840F62080D9F84DF928C9C10C8D';
-$relativePath = 'RODEConnect_Full (1.3.44).msi';
+$checksum = '32DD2BAA5C68A4419BB363EEDDB645192FE14073AC77F1923B3534B5E8F2D5EC';
+$relativePath = 'RODEConnect_Full (1.3.47).msi';
 
 
 if ([Version] (Get-CimInstance Win32_OperatingSystem).Version -lt [version] "10.0.17134.0") {
