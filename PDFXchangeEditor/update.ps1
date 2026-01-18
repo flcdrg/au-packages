214a22cdf77d0f522e82032bdb04c7a56eeaa41a
David Gardiner <david@gardinerfamily.au>
2026-01-18 16:23:47 +1030

    Fix update.ps1 for PDFXchange packages

--- PDFXchangeEditor/update.ps1
+++ PDFXchangeEditor/update.ps1
@@ -1,15 +1,11 @@
 Import-Module chocolatey-au
+#Import-Module "$PSScriptRoot\..\..\chocolatey-au\src\chocolatey-au.psm1"
 
 function global:au_SearchReplace {
-    $d32 = [DateTimeOffset] $Latest.LastModified32
-    $d64 = [DateTimeOffset] $Latest.LastModified64
-
     @{
         'tools\chocolateyInstall.ps1' = @{
             "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
             "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-            "(^[$]lastModified32\s*=\s*)(.*)(\s+\#)" = "`$1New-Object -TypeName DateTimeOffset $($d32.Year), $($d32.Month), $($d32.Day), $($d32.Hour), $($d32.Minute), $($d32.Second), 0`$3"
-            "(^[$]lastModified64\s*=\s*)(.*)(\s+\#)" = "`$1New-Object -TypeName DateTimeOffset $($d64.Year), $($d64.Month), $($d64.Day), $($d64.Hour), $($d64.Minute), $($d64.Second), 0`$3"
             "(^[$]filename\s*=\s*)('.*')" = "`$1'$($Latest.Filename32)'"
             "(^[$]filename64\s*=\s*)('.*')" = "`$1'$($Latest.Filename64)'"
             "(^[$]url\s*=\s*)('.*')" = "`$1'$($Latest.url)'"
@@ -42,23 +38,24 @@ function global:au_GetLatest {
         $x64update = $xml.SelectNodes("//t:bundle[@id='$bundleId']/t:update", $xmlNameSpace) | Sort-Object @{expr={[version]$_.version}; desc=$true} | Select-Object -First 1
         $filename64 = $x64update.url
 
-        $primaryDownloadUrl = "https://downloads.pdf-xchange.com/$filename"
-        $primaryDownloadUrl64 = "https://downloads.pdf-xchange.com/$filename64"
+        $primaryDownloadUrl = "https://downloads.pdf-xchange.com/$version/$filename"
+        $primaryDownloadUrl64 = "https://downloads.pdf-xchange.com/$version/$filename64"
 
         $response = Invoke-WebRequest $primaryDownloadUrl -Method Head -UseBasicParsing
-        $lastModifiedHeader = $response.Headers.'Last-Modified'
-        $x86lastModified = [DateTimeOffset]::Parse($lastModifiedHeader, [Globalization.CultureInfo]::InvariantCulture)
+
+        if ($response.StatusCode -ne 200) {
+            throw "Failed to access $primaryDownloadUrl"
+        }
 
         $response = Invoke-WebRequest $primaryDownloadUrl64 -Method Head -UseBasicParsing
-        $lastModifiedHeader = $response.Headers.'Last-Modified'
-        $x64lastModified = [DateTimeOffset]::Parse($lastModifiedHeader, [Globalization.CultureInfo]::InvariantCulture)
+        if ($response.StatusCode -ne 200) {
+            throw "Failed to access $primaryDownloadUrl64"
+        }
 
         $Latest = @{ 
             Version = $version
             Checksum32 = $x32update.hash
             Checksum64 = $x64update.hash
-            LastModified32 = $x86lastModified
-            LastModified64 = $x64lastModified
             Filename32 = $filename
             Filename64 = $filename64
             url = $primaryDownloadUrl
