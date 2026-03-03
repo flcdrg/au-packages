313256c229c91957e547b49d45eba05a3a0d3552
David Gardiner <david@gardinerfamily.au>
2026-03-03 00:08:45 +0000

    AU-dotnet: 2 updated [skip ci]

--- dell-system-update/tools/chocolateyinstall.ps1
+++ dell-system-update/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64 = 'https://downloads.dell.com/FOLDER14010777M/1/Systems-Management_Application_F3T5C_WN64_2.2.0.0_A00.EXE'
-$checksum64 = '87d08c86783ed3a0ac7f213956a8c973'
+$url64 = 'https://downloads.dell.com/FOLDER14217017M/1/Systems-Management_Application_RXKJ5_WN64_2.2.0.1_A00.EXE'
+$checksum64 = '98bd636e3b806f6fb0820a6abf4c26bf'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
