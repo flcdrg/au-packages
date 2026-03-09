e5bf6716493a0c6bbc213ebc03080cdcaefaa336
David Gardiner <david@gardinerfamily.au>
2026-03-09 14:55:54 +1030

    Change 'ssms' to 'Ssms'

--- sql-server-management-studio/tools/chocolateyinstall.ps1
+++ sql-server-management-studio/tools/chocolateyinstall.ps1
@@ -4,14 +4,14 @@ $fullUrl = 'https://download.visualstudio.microsoft.com/download/pr/2b23517b-e10
 $fullChecksum = '11AFC06E10A3E19EC60F9CA7B768ED68B9A74D8FCFF5F6ACAC275127BA60C965'
 
 Install-VisualStudio `
-    -PackageName 'sql-server-management-studio' `
-    -ApplicationName 'SQL Server Management Studio' `
-    -Url $fullUrl `
-    -Checksum $fullChecksum `
-    -ChecksumType 'SHA256' `
-    -InstallerTechnology 'WillowVS2017OrLater' `
-    -DefaultParameterValues @{
-      channelId = 'SSMS.22.SSMS.Release'
-      channelUri = 'https://aka.ms/ssms/22/release/channel'
-    } `
-    -Product 'ssms'
+  -PackageName 'sql-server-management-studio' `
+  -ApplicationName 'SQL Server Management Studio' `
+  -Url $fullUrl `
+  -Checksum $fullChecksum `
+  -ChecksumType 'SHA256' `
+  -InstallerTechnology 'WillowVS2017OrLater' `
+  -DefaultParameterValues @{
+  channelId  = 'SSMS.22.SSMS.Release'
+  channelUri = 'https://aka.ms/ssms/22/release/channel'
+} `
+  -Product 'Ssms'
