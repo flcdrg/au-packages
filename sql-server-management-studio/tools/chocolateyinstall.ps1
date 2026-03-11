a189badbe8a8e7b787896d09066133cfcf7b552b
David Gardiner <david@gardinerfamily.au>
2026-03-11 00:06:24 +0000

    AU-dotnet: 1 updated [skip ci]

--- sql-server-management-studio/tools/chocolateyinstall.ps1
+++ sql-server-management-studio/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$fullUrl = 'https://download.visualstudio.microsoft.com/download/pr/2b23517b-e100-42e1-a560-063af6edc4ec/11afc06e10a3e19ec60f9ca7b768ed68b9a74d8fcff5f6acac275127ba60c965/vs_SSMS.exe'
-$fullChecksum = '11AFC06E10A3E19EC60F9CA7B768ED68B9A74D8FCFF5F6ACAC275127BA60C965'
+$fullUrl = 'https://download.visualstudio.microsoft.com/download/pr/c7f9c40d-5956-4fa0-bd04-b866c2461198/58efa2590bf79fcea84f35cb6d0c5f6b852f7aa21a8e40939d1fdfe9cba80650/vs_SSMS.exe'
+$fullChecksum = '58EFA2590BF79FCEA84F35CB6D0C5F6B852F7AA21A8E40939D1FDFE9CBA80650'
 
 Install-VisualStudio `
   -PackageName 'sql-server-management-studio' `
