c05d341ae16ebfeeada9a0b042ffa64de2e622bc
David Gardiner <david@gardinerfamily.au>
2026-04-24 00:18:04 +0000

    AU-dotnet: 1 updated [skip ci]

--- ndi-tools/tools/chocolateyinstall.ps1
+++ ndi-tools/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
 $url = 'https://downloads.ndi.tv/Tools/NDI%206%20Tools.exe'
-$checksum = '5B7BCA109A9E16EE58F013F1FB0FCC7E2F03E603B436446FBC356E1D7F51D6BC'
+$checksum = '605CB6128999778B38126FFEF167C76D3D8648D39A582FFC71529DD26F64BF32'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
